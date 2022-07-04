574668a73f3e475ac8ff7a6437a08ec346d051df
pradana4648 <wakwaujaim@gmail.com>
2022-07-04 19:41:41 +0700

    initial commit

---
+++ src/test/java/com/belajar/springtutorial/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.springtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
