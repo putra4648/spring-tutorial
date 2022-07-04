ee3efab94249f82d2e7a00d7d5088d1e0cad422e
pradana4648 <wakwaujaim@gmail.com>
2022-07-04 20:03:00 +0700

    tambah dokumentasi untuk singleton

---
+++ src/test/java/com/belajar/springtutorial/Database.java
@@ -0,0 +1,20 @@
+package com.belajar.springtutorial;
+
+// Konsep singleton
+public class Database {
+    // Membuat private field
+    private static Database database;
+
+    // Membuat fungsi getInstance()
+    // yang mengreturn nilai dari private 'Database' konstruktor
+    public static Database getInstance() {
+        if (database == null) {
+            database = new Database();
+        }
+        return database;
+    }
+
+    // Mengubah default konstruktor dari public ke private
+    private Database() {
+    }
+}
